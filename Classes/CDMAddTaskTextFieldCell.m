9097a816e11b3a9d8017cfab7fb2c645ee9dd448
Sam Soffes <sam@samsoff.es>
2012-08-03 05:45:19 -0700

    Improve layout

--- Classes/CDMAddTaskTextFieldCell.m
+++ Classes/CDMAddTaskTextFieldCell.m
@@ -9,17 +9,24 @@
 #import "CDMAddTaskTextFieldCell.h"
 #import "NSImage+CDMAdditions.h"
 
-// Inspired by http://stackoverflow.com/a/8626071/118631
-
 @implementation CDMAddTaskTextFieldCell {
 	BOOL _selected;
 }
 
+- (void)awakeFromNib {
+	[super awakeFromNib];
+	self.font = [NSFont fontWithName:kCDIRegularFontName size:16.0];
+	self.textColor = [NSColor colorWithCalibratedRed:0.200 green:0.200 blue:0.200 alpha:1];
+	self.placeholderString = @"What do you have to do?";
+}
+
+
+// From http://stackoverflow.com/a/8626071/118631
 - (NSRect)adjustedFrameToVerticallyCenterText:(NSRect)frame {
 	NSInteger offset = floor((NSHeight(frame) - ([[self font] ascender] - [[self font] descender])) / 2);
 	NSRect rect = NSInsetRect(frame, 0.0, offset);
 	rect.origin.x += 10.0f;
-	rect.origin.y -= 1.0f;
+	rect.origin.y += 1.0f;
 	rect.size.width -= 20.0f;
 	return rect;
 }
@@ -44,9 +51,18 @@
 
 
 - (void)drawInteriorWithFrame:(NSRect)frame inView:(NSView *)view {
+	// Background
 	NSImage *image = _selected ? [NSImage imageNamed:@"textfield-focused"] : [NSImage imageNamed:@"textfield"];
 	[image drawStretchableImageInRect:frame leftCapWidth:8.0f];
 	
+	// Placeholder color
+	if (self.stringValue.length == 0) {
+		[[NSColor colorWithCalibratedRed:0.624 green:0.635 blue:0.675 alpha:1] set];
+	} else {
+		[self.textColor set];
+	}
+	
+	// Draw text
 	[super drawInteriorWithFrame:[self adjustedFrameToVerticallyCenterText:frame] inView:view];
 }
 
