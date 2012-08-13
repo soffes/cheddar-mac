57c4294cac641754a7c9d462b4e0e4f4effb2e4f
Indragie Karunaratne <indragie@me.com>
2012-08-13 13:17:59 -0600

    More work on the text field

--- Classes/CDMTextFieldCell.m
+++ Classes/CDMTextFieldCell.m
@@ -7,8 +7,11 @@
 //
 
 #import "CDMTextFieldCell.h"
+#import "NSBezierPath+MCAdditions.h"
 
 static CGFloat const kCDMTextFieldCellXInset = 10.0f;
+static CGFloat const kCDMTextFieldCellCornerRadius = 4.0f;
+static CGFloat const kCDMTextFieldCellShadowBlurRadius = 4.0f;
 
 @implementation CDMTextFieldCell
 - (void)awakeFromNib {
@@ -18,6 +21,7 @@ static CGFloat const kCDMTextFieldCellXInset = 10.0f;
     [self setDrawsBackground:NO];
     [self setBordered:NO];
     [self setBezeled:NO];
+    [self setFocusRingType:NSFocusRingTypeNone];
 }
 
 // From http://stackoverflow.com/a/8626071/118631
@@ -36,6 +40,28 @@ static CGFloat const kCDMTextFieldCellXInset = 10.0f;
 
 - (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
 {
+    NSColor *borderColor = [NSColor colorWithDeviceRed:0.72 green:0.73 blue:0.76 alpha:1.0];
+    NSColor *fillColor = [NSColor whiteColor];
+    NSColor *shadowColor = [NSColor colorWithDeviceWhite:0.f alpha:0.3f];
+    NSRect drawingRect = NSInsetRect(cellFrame, 1.f, 1.f);
+    NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:drawingRect xRadius:kCDMTextFieldCellCornerRadius yRadius:kCDMTextFieldCellCornerRadius];
+    [borderColor setStroke];
+    [fillColor setFill];
+    [path fill];
+    [path stroke];
+    NSShadow *shadow = [NSShadow new];
+    [shadow setShadowBlurRadius:kCDMTextFieldCellShadowBlurRadius];
+    [shadow setShadowColor:shadowColor];
+    NSRect shadowRect = drawingRect;
+    // Add space on each of the dimensions that we don't want the shadow showing through
+    shadowRect.size.height += 20.f;
+    shadowRect.origin.x -= 20.f;
+    shadowRect.size.width += 40.f;
+    NSBezierPath *shadowPath = [NSBezierPath bezierPathWithRect:shadowRect];
+    [NSGraphicsContext saveGraphicsState];
+    [path addClip];
+    [shadowPath fillWithInnerShadow:shadow];
+    [NSGraphicsContext restoreGraphicsState];
     [super drawInteriorWithFrame:[self adjustedFrameToVerticallyCenterText:cellFrame] inView:controlView];
 }
 @end
