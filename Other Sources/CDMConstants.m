1b09c9931d65c39311087b245259c4adc80ae21c
Sam Soffes <sam@samsoff.es>
2012-08-13 19:14:58 -0700

    Add constants

---
+++ Other Sources/CDMConstants.m
@@ -0,0 +1,11 @@
+//
+//  CDMConstants.m
+//  Cheddar for Mac
+//
+//  Created by Sam Soffes on 8/13/12.
+//  Copyright (c) 2012 Nothing Magical. All rights reserved.
+//
+
+#import "CDMConstants.h"
+
+CGFloat const kCDMTableViewAnimationDuration = 0.2f;
