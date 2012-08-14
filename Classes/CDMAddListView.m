0bb5ff140d491b594676fe52b4659e5a3c43e7f6
Indragie Karunaratne <indragie@me.com>
2012-08-14 00:14:32 -0600

    Starting work on add list view

---
+++ Classes/CDMAddListView.m
@@ -0,0 +1,28 @@
+//
+//  CDMAddListView.m
+//  Cheddar for Mac
+//
+//  Created by Indragie Karunaratne on 2012-08-13.
+//  Copyright (c) 2012 Nothing Magical. All rights reserved.
+//
+
+#import "CDMAddListView.h"
+
+@implementation CDMAddListView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
