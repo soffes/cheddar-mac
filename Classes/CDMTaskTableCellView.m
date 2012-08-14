ee916c85010cf39830fda93544239e8aa03a5a0c
Sam Soffes <sam@samsoff.es>
2012-08-13 19:26:08 -0700

    Improve task cell

--- Classes/CDMTaskTableCellView.m
+++ Classes/CDMTaskTableCellView.m
@@ -7,6 +7,7 @@
 //
 
 #import "CDMTaskTableCellView.h"
+#import "NSColor+CDMAdditions.h"
 
 @implementation CDMTaskTableCellView
 
@@ -14,6 +15,18 @@
 	[super awakeFromNib];
 	self.textField.font = [NSFont fontWithName:kCDMRegularFontName size:15.0];
 	self.textField.textColor = [NSColor colorWithCalibratedRed:0.200 green:0.200 blue:0.200 alpha:1];
+}
+
+
+- (void)setObjectValue:(id)objectValue {
+	[super setObjectValue:objectValue];
+
+	CDKTask *task = (CDKTask *)objectValue;
+	if (task.completed) {
+		self.textField.textColor = [NSColor cheddarSteelColor];
+	} else {
+		self.textField.textColor = [NSColor colorWithCalibratedRed:0.200 green:0.200 blue:0.200 alpha:1];
+	}
 }
 
 @end
