19f579e93fe86c1b6c58734612cc554b999e2f09
Indragie Karunaratne <indragie@me.com>
2012-08-13 21:20:44 -0600

    Update text colour when task checked

---
+++ Classes/CDMBoolToTaskTextColorValueTransformer.h
@@ -0,0 +1,13 @@
+//
+//  CDMBoolToTaskTextColorValueTransformer.h
+//  Cheddar for Mac
+//
+//  Created by Indragie Karunaratne on 2012-08-13.
+//  Copyright (c) 2012 Nothing Magical. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CDMBoolToTaskTextColorValueTransformer : NSValueTransformer
+
+@end
---
+++ Classes/CDMBoolToTaskTextColorValueTransformer.m
@@ -0,0 +1,18 @@
+//
+//  CDMBoolToTaskTextColorValueTransformer.m
+//  Cheddar for Mac
+//
+//  Created by Indragie Karunaratne on 2012-08-13.
+//  Copyright (c) 2012 Nothing Magical. All rights reserved.
+//
+
+#import "CDMBoolToTaskTextColorValueTransformer.h"
+#import "NSColor+CDMAdditions.h"
+
+@implementation CDMBoolToTaskTextColorValueTransformer
++ (Class)transformedValueClass { return [NSColor class]; }
++ (BOOL)allowsReverseTransformation { return NO; }
+- (id)transformedValue:(id)value {
+    return [value boolValue] ? [NSColor cheddarSteelColor] : [NSColor colorWithCalibratedRed:0.200 green:0.200 blue:0.200 alpha:1];
+}
+@end
