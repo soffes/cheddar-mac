bad4b813047bd9180e6d0d9680dc008de642b4a1
Indragie Karunaratne <indragie@me.com>
2012-08-13 22:20:22 -0600

    Added shadow to bottom of table views

---
+++ Classes/CDMShadowTableView.m
@@ -0,0 +1,28 @@
+//
+//  CDMShadowTableView.m
+//  Cheddar for Mac
+//
+//  Created by Indragie Karunaratne on 2012-08-13.
+//  Copyright (c) 2012 Nothing Magical. All rights reserved.
+//
+
+#import "CDMShadowTableView.h"
+
+@implementation CDMShadowTableView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
