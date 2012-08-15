7d6f254bc568368c0823c8fe3c09f37b6e35e588
Indragie Karunaratne <indragie@me.com>
2012-08-15 16:25:32 -0600

    Inactive state for tag bar

--- Classes/CDMTagFilterBar.m
+++ Classes/CDMTagFilterBar.m
@@ -10,20 +10,46 @@
 
 #define CDMTagFilterBarBottomColor [NSColor colorWithCalibratedRed:0.071 green:0.570 blue:0.801 alpha:1.000]
 #define CDMTagFilterBarTopColor [NSColor colorWithCalibratedRed:0.082 green:0.654 blue:0.887 alpha:1.000]
+#define CDMTagFilterBarInactiveTopColor [NSColor colorWithCalibratedRed:0.710 green:0.705 blue:0.710 alpha:1.000] 
+#define CDMTagFilterBarInactiveBottomColor [NSColor colorWithCalibratedWhite:0.542 alpha:1.000]
+
+@interface CDMTagFilterBar ()
+- (void)_redrawView;
+@end
 
 @implementation CDMTagFilterBar
 @synthesize delegate = _delegate;
 
-- (void)drawRect:(NSRect)dirtyRect
-{
-    NSGradient *gradient = [[NSGradient alloc] initWithStartingColor:CDMTagFilterBarBottomColor endingColor:CDMTagFilterBarTopColor];
+- (id)initWithFrame:(NSRect)frameRect {
+    if ((self = [super initWithFrame:frameRect])) {
+        NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+        [nc addObserver:self selector:@selector(_redrawView) name:NSWindowDidBecomeKeyNotification object:[self window]];
+        [nc addObserver:self selector:@selector(_redrawView) name:NSWindowDidResignKeyNotification object:[self window]];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    BOOL active = [[self window] isKeyWindow] && [NSApp isActive];
+    NSColor *topColor = active ? CDMTagFilterBarTopColor : CDMTagFilterBarInactiveTopColor;
+    NSColor *bottomColor = active ? CDMTagFilterBarBottomColor : CDMTagFilterBarInactiveBottomColor;
+    NSGradient *gradient = [[NSGradient alloc] initWithStartingColor:bottomColor endingColor:topColor];
     [gradient drawInRect:[self bounds] angle:90.f];
 }
 
-- (void)mouseDown:(NSEvent *)theEvent
-{
+- (void)mouseDown:(NSEvent *)theEvent {
     if ([self.delegate respondsToSelector:@selector(tagFilterBarClicked:)]) {
         [self.delegate tagFilterBarClicked:self];
     }
+}
+
+#pragma mark - Private
+
+- (void)_redrawView {
+    [self setNeedsDisplay:YES];
 }
 @end
