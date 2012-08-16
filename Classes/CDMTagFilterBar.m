22fcfc374181c88ce1fb6463357fbd91aa987632
Sam Soffes <sam@samsoff.es>
2012-08-15 18:33:16 -0700

    Prettier tags

--- Classes/CDMTagFilterBar.m
+++ Classes/CDMTagFilterBar.m
@@ -29,17 +29,36 @@
     return self;
 }
 
+
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
+
 
 - (void)drawRect:(NSRect)dirtyRect {
     BOOL active = [[self window] isKeyWindow] && [NSApp isActive];
     NSColor *topColor = active ? CDMTagFilterBarTopColor : CDMTagFilterBarInactiveTopColor;
     NSColor *bottomColor = active ? CDMTagFilterBarBottomColor : CDMTagFilterBarInactiveBottomColor;
+	NSColor *bottomInsetColor = active ? [NSColor colorWithCalibratedRed:0.370 green:0.687 blue:0.834 alpha:1.0] : [NSColor colorWithCalibratedWhite:0.585 alpha:1.0];
+	NSColor *bottomBorderColor = active ? [NSColor colorWithCalibratedRed:0.083 green:0.427 blue:0.641 alpha:1.0] : [NSColor colorWithCalibratedWhite:0.4 alpha:1.0];
+	
     NSGradient *gradient = [[NSGradient alloc] initWithStartingColor:bottomColor endingColor:topColor];
     [gradient drawInRect:[self bounds] angle:90.f];
+
+	CGSize size = self.bounds.size;
+	if (active) {
+		NSColor *topInsetColor = [NSColor colorWithCalibratedRed:0.453 green:0.770 blue:0.911 alpha:1.0];
+		[topInsetColor setFill];
+		[NSBezierPath fillRect:CGRectMake(0.0f, size.height - 1.0f, size.width, 1.0f)];
+	}
+	
+	[bottomInsetColor setFill];
+	[NSBezierPath fillRect:CGRectMake(0.0f, 1.0f, size.width, 1.0f)];
+
+	[bottomBorderColor setFill];
+	[NSBezierPath fillRect:CGRectMake(0.0f, 0.0f, size.width, 1.0f)];
 }
+
 
 - (void)mouseDown:(NSEvent *)theEvent {
     if ([self.delegate respondsToSelector:@selector(tagFilterBarClicked:)]) {
@@ -47,9 +66,11 @@
     }
 }
 
+
 #pragma mark - Private
 
 - (void)_redrawView {
     [self setNeedsDisplay:YES];
 }
+
 @end
