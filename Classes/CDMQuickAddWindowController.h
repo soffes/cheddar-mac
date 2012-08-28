a0c8df93b29b9fe62b46e899fd77aab521c4f42e
Indragie Karunaratne <i@indragie.com>
2012-08-27 23:21:40 -0600

    Added basic quick add window NIB

---
+++ Classes/CDMQuickAddWindowController.h
@@ -0,0 +1,13 @@
+//
+//  CDMQuickAddWindowController.h
+//  Cheddar for Mac
+//
+//  Created by Indragie Karunaratne on 2012-08-27.
+//  Copyright (c) 2012 Nothing Magical. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CDMQuickAddWindowController : NSWindowController
+
+@end
---
+++ Classes/CDMQuickAddWindowController.m
@@ -0,0 +1,34 @@
+//
+//  CDMQuickAddWindowController.m
+//  Cheddar for Mac
+//
+//  Created by Indragie Karunaratne on 2012-08-27.
+//  Copyright (c) 2012 Nothing Magical. All rights reserved.
+//
+
+#import "CDMQuickAddWindowController.h"
+
+@interface CDMQuickAddWindowController ()
+
+@end
+
+@implementation CDMQuickAddWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
